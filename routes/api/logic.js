664e08ea4e21a3c6a207eec918405399176dab8b
unknown <guruluckystacker@gmail.com>
2022-02-18 00:31:04 -0300

    a

--- routes/api/logic.js
+++ routes/api/logic.js
@@ -5,7 +5,8 @@ const keccak256 = require('keccak256')
 const WhiteList = [
     "0xc5Da0445c213CF71E04ad5af2E14d7E9bF114f7E",
     "0x73eA2Fdc2315f5ac9AdDA2f815F4aF8e52D9Bf4d",
-    "0x3E8A9DA88aEAb7EcE3cD8f90227Dab855BFDc716"
+    "0x3E8A9DA88aEAb7EcE3cD8f90227Dab855BFDc716",
+    "0xcF512b03e0c81eBfeCe32d89fD62aFdEc0C3E234"
 ]
 
 const leafNodes = WhiteList.map(addr => keccak256(addr))
